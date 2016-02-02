baec134ca6ef6f7dd8c63f605e7feb911b84b0aa
Louie Hernandez <Louie Hernandez@10.0.0.145>
2016-02-01 18:19:44 -0600

    URL class

---
+++ twitterME/src/twitterME/URL.java
@@ -0,0 +1,16 @@
+package twitterME;
+
+public class URL {
+	String Parse(String tweet){
+		//search for URL
+	    if (tweet.contains('http:' || '.com')) {
+	        int indexOfHttp = tweet.indexOf('http:');
+	        int endPoint = (tweet.indexOf(' ', indexOfHttp) != -1) ?
+	        	tweet.indexOf(' ', indexOfHttp) : tweet.length();
+	        String url = tweet.substring(indexOfHttp, endPoint);       
+	    }
+	 
+	    //final
+	    return tweet;
+	    }
+}
