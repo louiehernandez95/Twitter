f68374e7ab55aa595298dc7852d75528774b0947
Louie Hernandez <Louie Hernandez@10.0.0.145>
2016-02-01 18:17:30 -0600

    Url revision

--- twitterME/src/twitterME/Main.java
+++ twitterME/src/twitterME/Main.java
@@ -1,18 +1,5 @@
 package twitterME;
 
 public class Main {
-String Parse(String tweet){
-	
-	
-	
-	//search for URL
-    if (tweet && tweet?.contains('http:')) {
-        int indexOfHttp = tweet.indexOf('http:');
-        int endPoint = (tweet.indexOf(' ', indexOfHttp) != -1) ?
-        	tweet.indexOf(' ', indexOfHttp) : tweet.length();
-        String url = tweet.substring(indexOfHttp, endPoint);
-        String targetUrlHtml=  "<a href='${url}' target='_blank'>${url}</a>";
-        tweet = tweet.replace(url,targetUrlHtml );
-    }
-    
+
 }
