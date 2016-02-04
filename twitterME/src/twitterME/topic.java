57026e360c7e92a23f107ed14eaf5a00a630b17a
Louie Hernandez <lhernandez6@luc.edu>
2016-02-03 23:43:20 -0600

    making twitter parsers set to a linkedlist

--- twitterME/src/twitterME/topic.java
+++ twitterME/src/twitterME/topic.java
@@ -4,44 +4,59 @@ import java.util.*;
 private LinkedList<String> lines = new LinkedList<String>();
 
 public class Parser {
-	public String FindTopic(String tweet){
+	public Boolean FindTopic(String tweet){
 		//search for topic
+		int i=0;
 		String topic;
+		do {
 	    if (tweet.contains('@') {
 	    int indexOfTopic = tweet.indexOf('@');
         int endPoint = (tweet.indexOf(' ', indexOfTopic) != -1) ?
 	        	tweet.indexOf(' ', indexOfTopic) : tweet.length();
 	        topic = tweet.substring(indexOfTopic, endPoint);    
-	    return topic;
-	    }
-	 
-	    return "";
+	        lines.set(i, topic); 
+	        return true;
+	       }
+	    i++;
+        } while (i<lines.size());
+	    return false;
 	 }
 	 
-	 public String FindHash(String tweet){
+	 public Boolean FindHash(String tweet){
 			//search for hashtag 
 			String hashTag=tweet;
+			int i=0;
+			do
+            {
 		    if (tweet.contains('#') {
 	            int indexOfHash = tweet.indexOf('#');
 	            int endPoint = (tweet.indexOf(' ', indexOfHash) != -1) ?
 		          	tweet.indexOf(' ', indexOfHash) : tweet.length();
 		        hashTag = tweet.substring(indexOfHash, endPoint); 
-		        return hashTag;
+		        lines.set(i, hashTag); 
+		        return true;
 		       }
-		  
-		    return ""
+		    i++;
+            } while (i<lines.size());
+		    return false;
 		 }
-	 public String FindUrl(String tweet){
+	 public Boolean FindUrl(String tweet){
 			//search for URL
 			String url=tweet;
+			int i=0;
+			do
+            {
+              
 		    if (tweet.contains("http://")) {
 		        int indexOfHttp = tweet.indexOf('http://');
 		        int endPoint = (tweet.indexOf(' ', indexOfHttp) != -1) ?
 		        	tweet.indexOf(' ', indexOfHttp) : tweet.length();
 		        url = tweet.substring(indexOfHttp, endPoint);  
-		        return url;
-		    }
-		    return "";
-		    }
-	 
+		        lines.set(i, url);   
+		        return true;
+		        }
+		    i++;
+            } while (i<lines.size());
+		    return false;
+    }
 }
