76c1bf97a2e39414dc7263ce6e78bc26b84b0e28
Louie Hernandez <lhernandez6@luc.edu>
2016-02-03 20:17:42 -0600

    merging classes

--- twitterME/src/twitterME/topic.java
+++ twitterME/src/twitterME/topic.java
@@ -1,6 +1,8 @@
-package twitterME;
+import java.io.*;
+import java.util.*;
 
-public class topic {
+
+public class Parser {
 	public String FindTopic(String tweet){
 		//search for topic
 		String topic;
@@ -14,4 +16,35 @@ public class topic {
 	 
 	    return "";
 	 }
+	 
+	 public String FindHash(String tweet){
+			//search for hashtag 
+			String hashTag=tweet;
+		    if (tweet.contains('#') {
+	            int indexOfHash = tweet.indexOf('#');
+	            int endPoint = (tweet.indexOf(' ', indexOfHash) != -1) ?
+		          	tweet.indexOf(' ', indexOfHash) : tweet.length();
+		        hashTag = tweet.substring(indexOfHash, endPoint); 
+		        return hashTag;
+		       }
+		  
+		    return ""
+		 }
+	 public String FindUrl(String tweet){
+			//search for URL
+			String url=tweet;
+		    if (tweet.contains("http://")) {
+		        int indexOfHttp = tweet.indexOf('http://');
+		        int endPoint = (tweet.indexOf(' ', indexOfHttp) != -1) ?
+		        	tweet.indexOf(' ', indexOfHttp) : tweet.length();
+		        url = tweet.substring(indexOfHttp, endPoint);  
+		        return url;
+		    }
+		    return "";
+		    }
+	 public static String punctuation(String s){
+	        String t ;
+	        t = s.replaceAll("[\\[\\]_:\"'`?;0-9;()-/.,*! ]", " ").toLowerCase();
+	        return t;
+	   }
 }
