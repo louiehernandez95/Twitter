9b75be4ab5c9502f54a9838dfc79aac8dd209f56
Louie Hernandez <lhernandez6@luc.edu>
2016-02-03 21:24:16 -0600

    Testing if both strings are equal, if so returns true

--- twitterME/src/twitterME/Test.java
+++ twitterME/src/twitterME/Test.java
@@ -1,5 +1,18 @@
-package twitterME;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class Test {
 
+   public void testAssertions() {
+      //test data
+      String str1 = new String ("@true");
+      String str2 = new String ("true");
+      String str3 = new String ("#true");
+      String str4 = new String ("https://true");
+      
+      //Check that two objects are equal
+      assertEquals(str1.FindTopic(), str2);
+      assertEquals(str3.FindHash(), str2);
+      assertEquals(str4.FindUrl(), str2);
+   }
 }
